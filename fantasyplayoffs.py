3aa437c53748434e1fce3c4a3a4c3f81487ec669
josteffes <96957142+josteffes@users.noreply.github.com>
2025-01-19 17:44:47 -0600

    Update fantasyplayoffs.py

--- fantasyplayoffs.py
+++ fantasyplayoffs.py
@@ -258,8 +258,8 @@ with tab4:
     st.subheader("Current NFL Game")
 
     # Default values for dropdowns
-    default_team1 = "Eagles"  # Default first NFL team
-    default_team2 = "Rams"  # Default second NFL team
+    default_team1 = "Bills"  # Default first NFL team
+    default_team2 = "Ravens"  # Default second NFL team
     default_round = "Divisional"  # Default round
 
     # Dropdown inputs for NFL teams and round
